359751669b47cbbb9d9dc7aded6db0a1f57bd6ed
Hemashree47 <hemashree1910@gmail.com>
2024-05-29 11:12:38 +0530

    first commit

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Logo = ({w,h}) => {
+  return (
+    <>
+       <div className='font-bold'>
+            <i>VidhyaSHopHere</i>
+        </div> 
+    </>
+  )
+}
+
+export default Logo
